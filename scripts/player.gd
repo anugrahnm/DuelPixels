c24dc43b6551e06ba7700224f32c618a440f6014
doubl-u <69413572+doubl-u@users.noreply.github.com>
2024-08-31 19:41:08 +0530

    player scene update 0.1
    added inbuilt character script with gravity and made a simple label text change function

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+@onready var player_label: Label = %player_label
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func ChangeText(string_string):
+	player_label.text = string_string
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,14 +14,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		ChangeText("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		match direction:
+			1:ChangeText("right")
+			-1:ChangeText("left")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
