b490df6c311b233ef9099754e890132873179c27
doubl-u <69413572+doubl-u@users.noreply.github.com>
2024-09-03 21:45:37 +0530

    failed script files + vim motions plugin (not enabled)
    before implementation of state machice

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-var moveset = moveset_data.new().ExtractData()
+var arr1 = moveset_data.new().ExtractData()
+var parse = arr1[0]
+var moveset = arr1[1]
 
 @onready var player_label: Label = %player_label
-#damn
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -19,7 +21,9 @@ func _ready() -> void:
 
 func UpdateCurrentMove(current_move) -> void:
 	
-	
+	print(parse)
+	#print(parse["N0"]["kick (low right kick)"][1]["N1"]["kick (quick mid right kick)"][1]["N2"]["jump (resets character orientation mid air)"])
+	#data["N0"][last_N0_value][1]["N1"][last_N1_value][1]["N2"][last_N2_value].append(temp3)
 	
 	if not "N0" in current_array:
 		current_array[0] = "N0"
@@ -30,7 +34,7 @@ func UpdateCurrentMove(current_move) -> void:
 	else:
 		current_array[4] = "N2"
 		current_array[5] = current_move
-	print(current_array)
+	#print(current_array)
 		
 	string_passed = ".".join(current_array)
 	ChangeText(string_passed)
