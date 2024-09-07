6c2e644bf1178f0092042fd54e791df0d2ca1776
FluffyXVII <fluffyxvii@gmail.com>
2024-09-07 13:31:55 +0530

    Fixed Bugs with the Pause Menu
    - Can now open settings from pause menu and go back to the menu from settings.
    - Removed Settings Options scene and addded its contents back to Settings scene.

--- scripts_f/pause_menu.gd
+++ scripts_f/pause_menu.gd
@@ -1,40 +1,54 @@
-extends Control
-@onready var pause_menu1 = $"."
+extends CanvasLayer
+
+@onready var menu = $pause_menu
+@onready var settings = $Settings
+
+enum state {playing, on_pause, in_cutscene, loading_screen}
+var states = state.playing
+
+var is_hidden: bool
+
+func _ready():
+	menu.hide()
+	is_hidden = true
 
 func resume():
 	get_tree().paused = false
-	pause_menu1.hide()
-	
-func pause():
+	menu.hide()
+	settings.Exit()
+
+func OnPause():
+	settings.Exit()
 	get_tree().paused = true
-	pause_menu1.show()
+	menu.show()
 
 func pause_menu():
 	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
 		print("escape")
-		pause()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
+		OnPause()
+	elif Input.is_action_just_pressed("escape") and !settings.check_if_hidden():
 		resume()
 		print("escape 2")
+	elif Input.is_action_just_pressed("escape") and settings.check_if_hidden():
+		OnPause()
 		
 func _physics_process(_delta):
 	pause_menu()
 
-
 func _on_resume_pressed():
 	resume()
+	print("resume")
 
 func _on_restart_pressed():
 	resume()
 	get_tree().reload_current_scene()
-	
-
 
 func _on_quit_pressed():
 	get_tree().quit()
 
-
 func _on_settings_pressed():
-	resume()
-	get_tree().change_scene_to_file("res://scenes_f/settings.tscn")
-	
+	menu.hide()
+	settings.Enter()
+
+func _on_back_pressed():
+	OnPause()
--- scripts_f/settings.gd
+++ scripts_f/settings.gd
@@ -1,4 +1,42 @@
 extends Control
+@onready var option_tabs = $"Option Tabs"
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes_f/level_1.tscn")
+var is_hidden: bool
+
+### This is unfinished baselevel code to set be able to use the fn current_level_state
+#const lever_state_options = {"inplay":"inplay","inpause":"inpause","inanimation":"inanimation"}
+#var current_level_state = null
+#
+#func get_current_level_state():
+	#if current_level_state == null:
+		#push_error("current_level_state does not have a value, it needs to be set to one of " + str(lever_state_options.values()))
+		#get_tree().quit()
+		#return "you should not be able to read this line"
+	#else:
+		#return current_level_state 
+#
+#func set_current_level_state(new_current_level_state) -> void:
+	#if new_current_level_state in lever_state_options:
+		#current_level_state = lever_state_options[new_current_level_state]
+	#else:
+		#push_error("[" + new_current_level_state + "] not in lever_state_options")
+####
+
+func _ready():
+	hide()
+	is_hidden = true
+	
+func Enter() -> void :
+	option_tabs.current_tab = 0
+	show()
+	is_hidden = true
+	
+func Exit() -> void :
+	option_tabs.current_tab = 0
+	hide()
+	is_hidden = false
+	
+func check_if_hidden() -> bool :
+	return is_hidden
+	
+	
