2d60cd708f4b04018c08d8632bb7c1b951e44610
doubl-u <69413572+doubl-u@users.noreply.github.com>
2024-09-09 03:30:23 +0530

    updated the keyinputs
    punch,kick,heavy,grab_interact,roll_block

--- scripts_w/Main_Manager.gd
+++ scripts_w/Main_Manager.gd
@@ -9,26 +9,26 @@ var data : Array
 
 
 func ExtractData():
-    var json_file_access = FileAccess.open(JSON_FILE_PATH, FileAccess.READ)
-    json_data = JSON.parse_string(json_file_access.get_as_text())
+	var json_file_access = FileAccess.open(JSON_FILE_PATH, FileAccess.READ)
+	json_data = JSON.parse_string(json_file_access.get_as_text())
 
 func FormatData():
-    for json_item in json_data:
-        if json_item["id"]:
-            data.append(Data_Handler.new(json_item))
+	for json_item in json_data:
+		if json_item["id"]:
+			data.append(Data_Handler.new(json_item))
 
 func SearchInData(key_name,value):
-    for item in data:
-        if item.all[key_name] == str(value):
-            print(item.all["id"]," : ",item.all["name"])
+	for item in data:
+		if item.all[key_name] == str(value):
+			print(item.all["id"]," : ",item.all["name"])
 
 func ShowNextMove(id:String):
-    for item in data:
-        if item.all["id"] == id:
-            print(item.all["id"]," > ",item.all["next_possible_move"])
+	for item in data:
+		if item.all["id"] == id:
+			print(item.all["id"]," > ",item.all["next_possible_move"])
 
 func _ready() -> void:
-    ExtractData()
-    FormatData()
-    #SearchInData("stamina","false")
-    #ShowNextMove("k2")
+	ExtractData()
+	FormatData()
+	#SearchInData("stamina","false")
+	#ShowNextMove("k2")
